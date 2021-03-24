fe01b188582f346ea9fac5d5cc6e29ebe424b85d
Vinicius Lima <viniciusalima1007@gmail.com>
2021-03-24 02:21:55 -0300

    bugfix noreferrer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ function Footer() {
             <p>
             Desenvolvido por Vinicius Lima.<br/><br/>
             
-                        <a target="_blank" href="https://github.com/ViniciusLima10"><i class="fab fa-github"></i></a>
-                        <a target="_blank" href="https://www.linkedin.com/in/vinicius-almeida-lima-3281771b1/"><i class="fab fa-linkedin-in"></i></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/ViniciusLima10"><i class="fab fa-github"></i></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/vinicius-almeida-lima-3281771b1/"><i class="fab fa-linkedin-in"></i></a>
         </p>
         </footer>
     )
